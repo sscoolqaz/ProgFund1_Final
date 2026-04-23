1b337e0afe83fad1706c67e112d30372595c6149
Sierra S <sscoolqaz@protonmail.com>
2026-04-23 18:40:12 -0500

    added initial struct

--- BusinessLogic.cpp
+++ BusinessLogic.cpp
@@ -1,55 +1,39 @@
 #include "BusinessLogic.h"
 #include "Project.h"
 #include "stdafx.h"
-#include <string>
-#include <vector>
+#include <invitee.h>
 
-// THIS IS EXAMPLE CODE - REPLACE WITH YOUR SOLUTION
-int Input1Handle;
-int Input2Handle;
-int ButtonHandle;
-int Label1Handle;
-int Label2Handle;
-int LabelOutput1Handle;
-int LabelOutput2Handle;
-int Output1Handle;
-int Output2Handle;
-// END OF EXAMPLE CODE
+struct GHWND {
+  struct {
+    int title;
+    int first;
+    int last;
+    int suffix;
+    int spouseTitle;
+    int spouseLast;
+    int spouseFirst;
+    int children;
+    int street;
+    int city;
+    int zip;
+    int inviteeType;
+    int eventState;
+  } input;
 
-void InitializeGUI(HWND hWnd) {
-  // THIS IS EXAMPLE CODE - REPLACE WITH YOUR SOLUTION
-  Label1Handle = CreateLabel(hWnd, "V1", 70, 100, 30, 20);
-  Input1Handle = CreateTextBox(hWnd, 100, 100, 400, 20, true, false, false);
-  Label1Handle = CreateLabel(hWnd, "V2", 70, 200, 30, 20);
-  Input2Handle = CreateTextBox(hWnd, 100, 200, 400, 20, true, false, false);
-  ButtonHandle = CreateButton(hWnd, "Do Math", 100, 300, 150, 30);
+  struct {
+    int inStateInvitation;
+    int inStateAnnouncement;
+    int outStateInvitation;
+    int outStateAnnouncement;
+    int local;
+    int distant;
+  } lists;
 
-  LabelOutput1Handle =
-      CreateLabel(hWnd, "Result of Multiply", 600, 80, 150, 20);
-  Output1Handle = CreateTextBox(hWnd, 600, 100, 400, 150, false, false, true);
-  LabelOutput2Handle = CreateLabel(hWnd, "Result of Sum", 600, 280, 150, 20);
-  Output2Handle = CreateTextBox(hWnd, 600, 300, 400, 150, false, true, false);
-  // END OF EXAMPLE CODE
-}
+  int button;
+};
 
-void ProcessInput() {
-  // THIS IS EXAMPLE CODE - REPLACE WITH YOUR SOLUTION
-  int Value1 = ReadIntFromTextBox(Input1Handle);
-  int Value2 = ReadIntFromTextBox(Input2Handle);
+GHWND handle;
 
-  if (ReadStringFromTextBox(Output1Handle) == "") {
-    WriteStringToTextBox(Output1Handle, to_string(Value1 * Value2));
-  } else {
-    string NewResult = ", ";
-    NewResult.append(to_string(Value1 * Value2));
-    AppendStringToTextBox(Output1Handle, NewResult);
-  }
+void InitializeGUI(HWND hWnd) {}
 
-  if (ReadStringFromTextBox(Output2Handle) == "") {
-    AppendStringToTextBox(Output2Handle, to_string(Value1 + Value2));
-  } else {
-    AddLineToTextBox(Output2Handle, to_string(Value1 + Value2));
-  }
-
-  // END OF EXAMPLE CODE
-}
+void ProcessInput() {}
